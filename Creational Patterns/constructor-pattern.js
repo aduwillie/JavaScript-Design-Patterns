584a663fe6f145584baa926bdcbf4fafd090d932
Williams Adu <aduwillie@gmail.com>
2017-07-13 08:10:02 +0000

    Added constructor pattern

--- Creational Patterns/constructor-pattern.js
+++ Creational Patterns/constructor-pattern.js
@@ -4,7 +4,25 @@ function Car(model, year, miles) {
     this.year = year;
     this.miles = miles;
 
+    // This object gets redefined for each new object created
+    // This method has to be shared among created objects
     this.toString = function() {
         return this.model + ' has done ' + this.miles + ' miles';
     }
 }
+
+
+// Constructors with prototypes
+function Car(model, year, miles) {
+    this.model = model;
+    this.year = year;
+    this.miles = miles;
+}
+
+// A single instance will be shared between all instances of Car
+Car.prototype.toString = function() {
+    return this.model + ' has done ' + this.miles + ' miles';
+}
+
+var civic = new Car('Honda', 2014, 1000);
+var corolla = new Car('Toyota', 2015, 2000);
